2b480aecd520530a57a969cc92423e3a5a3e656e
soccert <talyagriffin2004@gmail.com>
2018-07-31 15:41:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,4 +9,12 @@ get '/name' do
   "my name is Talya"
   end
   
+  get '/hometown' do
+    "my hometown is durham"
+  end
+  
+  get '/favorite-song' do
+    "my favorite song is lucid dreams"
+  end
+  
 end
